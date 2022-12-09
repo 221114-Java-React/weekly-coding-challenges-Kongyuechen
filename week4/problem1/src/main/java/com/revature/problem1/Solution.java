67ac43308ab415168ba662b6688d5ace64a54d27
kongyuechen <kongyue.chen@gmail.com>
2022-12-09 11:07:51 -0500

    p1 and 2

--- week4/problem1/src/main/java/com/revature/problem1/Solution.java
+++ week4/problem1/src/main/java/com/revature/problem1/Solution.java
@@ -1,7 +1,28 @@
 package com.revature.problem1;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Solution {
     public int elementNotInArray(int[] arr, int k) {
-        throw new RuntimeException("TODO! Needs implementation...");
+        boolean notFound = true;
+        ArrayList newArray = new ArrayList<>();
+        for (int i = 0; i < arr.length; i++) {
+            newArray.add(arr[i]);
+        }
+
+
+        while(notFound == true) {
+            k = k+1;
+            System.out.println(k);
+            boolean doesContain = newArray.contains(k);
+            System.out.println(doesContain);
+
+            if(doesContain == false) {
+                notFound = false;
+            }
+
+        }
+        return k;
     }
 }
--- week4/problem2/src/main/java/com/revature/problem2/Solution.java
+++ week4/problem2/src/main/java/com/revature/problem2/Solution.java
@@ -6,7 +6,23 @@ import java.util.Collections;
 import java.util.List;
 
 public class Solution {
+
+
     public List<String> autoComplete(List<String> list) {
-        throw new RuntimeException("TODO! Needs implementation...");
+        List<String> newList = new ArrayList<>();
+        for (int i = 1; i < list.size(); i++) {
+            newList.add(list.get(i));
+        }
+
+        ArrayList ans = new ArrayList<>();
+        int count = 0;
+        String prefix = list.get(0);
+        int pLen = prefix.length();
+        for(String ele: newList){
+            if(ele.indexOf(prefix) == 0) {
+                ans.add(ele);
+            }
+        }
+        return ans;
     }
 }
